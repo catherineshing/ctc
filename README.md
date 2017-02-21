04c51f8eea2f0bdf57e8070d9b653a79fc33e5a5
Catherine Shing <catherine.shing@reputation.com>
2017-02-21 12:33:27 -0800

    adding swipe functions

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # ctc
 
+install node 4.x
+
 npm install -g nodemon
 npm install -g ejs
+npm install -g bower
 
 git clone https://github.com/catherineshing/ctc
 cd ctc
--- app/src/client/item/item-view.js
+++ app/src/client/item/item-view.js
@@ -2,15 +2,17 @@
     'use strict';
 
     angular.module('ctc.item-view', [
+        'ngTouch',
         'ctc.ctc-service'
     ])
 
         .controller('ItemViewController', [
+            '$location',
             '$scope',
             '$stateParams',
             '$window',
             'CtcService',
-            function($scope, $stateParams, $window, CtcService) {
+            function($location, $scope, $stateParams, $window, CtcService) {
                 var that = this;
 
                 angular.element($window).off('scroll');
@@ -22,6 +24,12 @@
                     .then(function(item) {
                         that.item = item;
                     });
+
+                this.goToItem = function(itemId) {
+                    if (itemId) {
+                        $location.path('/' + that.parentState + '/item/' + itemId);
+                    }
+                };
             }
         ]);
 
